035a4b47c5136e369ccf76deb1715a1bc6c08ac9
Wenxin Liu <wenxinliudevops@gmail.com>
2023-05-25 04:00:11 +0100

    Improve metrics registry and recording metrics

--- src/KerasBinaryClassificationMLP.py
+++ src/KerasBinaryClassificationMLP.py
@@ -6,9 +6,10 @@ from src.Model import Model
 
 
 class KerasBinaryClassificationMLP(Model):
-    def __init__(self, optimiser, loss, hidden_layers, width, input_dimensions, output_dimensions):
+    def __init__(self, optimiser, loss, hidden_layers, width, input_dimensions, output_dimensions, metrics_registry):
         super().__init__()
         self.model = keras.Sequential()
+        self.metrics_registry = metrics_registry
         self.optimiser = optimiser
         self.loss = loss
         self.hidden_layers = hidden_layers
@@ -49,13 +50,14 @@ class KerasBinaryClassificationMLP(Model):
     def test(self):
         pass
 
+    # TODO: improve inference()
     def inference(self, input_value, verbose_mode=1):
         y_pred = []
 
-        for x in range(input_value):
+        for x in range(input_value.shape[0]):
             row = []
 
-            for y in range(input_value):
+            for y in range(input_value.shape[1]):
                 value = (self.model.predict(
                     np.array(([[x, y]])),
                     verbose=self.enable_verbose_output(verbose_mode)
@@ -71,15 +73,36 @@ class KerasBinaryClassificationMLP(Model):
         y_pred_copy = np.copy(y_pred)
         y_pred_flatten = []
 
-        for x in range(input_value):
+        for x in range(input_value.shape[0]):
             row = []
 
-            for y in range(input_value):
+            for y in range(input_value.shape[1]):
                 row.append(y_pred_copy[x, y, 0, 0])
 
             y_pred_flatten.append(row)
 
         y_pred_flatten = np.asarray(y_pred_flatten)
+
+        # register metrics for model performance
+        self.metrics_registry.reset_metrics()
+
+        self.metrics_registry.register_counter_metric("true_negative")
+        self.metrics_registry.register_counter_metric("true_positive")
+        self.metrics_registry.register_counter_metric("false_negative")
+        self.metrics_registry.register_counter_metric("false_positive")
+
+        for row in range(input_value.shape[0]):
+            for col in range(input_value.shape[1]):
+                if y_pred_flatten[row, col] == 0 and input_value[row, col] == 0:
+                    self.metrics_registry.increment_counter_metric("true_negative")
+                elif y_pred_flatten[row, col] == 1 and input_value[row, col] == 1:
+                    self.metrics_registry.increment_counter_metric("true_positive")
+                elif y_pred_flatten[row, col] == 0 and input_value[row, col] == 1:
+                    self.metrics_registry.increment_counter_metric("false_negative")
+                elif y_pred_flatten[row, col] == 1 and input_value[row, col] == 0:
+                    self.metrics_registry.increment_counter_metric("false_positive")
+                else:
+                    raise ValueError("data error, unknown y_pred and y_target combination")
 
         return y_pred_flatten
 
--- src/MetricsRegistry.py
+++ src/MetricsRegistry.py
@@ -2,27 +2,24 @@ class MetricsRegistry:
     def __int__(self):
         self.metrics_registry = {}
 
-    def register_metric(self, name, default_value):
-        self.metrics_registry[name] = default_value
+    def register_counter_metric(self, name):
+        self.metrics_registry[name] = 0
 
-    def record_metrics(self, y_pred, y_target):
-        self.register_metric("true_negative", 0)
-        self.register_metric("true_positive", 0)
-        self.register_metric("false_negative", 0)
-        self.register_metric("false_positive", 0)
-
-        for row in range(y_pred.shape[0]):
-            for col in range(y_pred.shape[1]):
-                if y_pred[row, col] == 0 and y_target[row, col] == 0:
-                    self.metrics_registry["true_negative"] += 1
-                elif y_pred[row, col] == 1 and y_target[row, col] == 1:
-                    self.metrics_registry["true_positive"] += 1
-                elif y_pred[row, col] == 0 and y_target[row, col] == 1:
-                    self.metrics_registry["false_negative"] += 1
-                elif y_pred[row, col] == 1 and y_target[row, col] == 0:
-                    self.metrics_registry["false_positive"] += 1
-                else:
-                    raise Exception("data error, unknown y_pred and y_target combination")
+    def get_metric(self, name):
+        return {name: self.metrics_registry[name]}
 
     def get_metrics(self):
         return self.metrics_registry
+
+    def increment_counter_metric(self, name):
+        if name in self.metrics_registry.keys():
+            self.metrics_registry[name] += 1
+        else:
+            raise ValueError("metric {} not registered".format(name))
+
+    def reset_metric(self, name):
+        self.metrics_registry[name] = 0
+
+    def reset_metrics(self):
+        for name in self.metrics_registry.keys():
+            self.metrics_registry[name] = 0
