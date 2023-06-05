9455da28019efd909e6835a4ea0b33ea0f10860c
Wenxin Liu <wenxinliudevops@gmail.com>
2023-06-05 01:46:47 +0100

    Implement 2D and 3D AABB

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,30 @@
+from src.data.DataLoader import DataLoader
 from src.metrics.MetricsRegistry import MetricsRegistry
+from src.model.boundingvolume.aabb.AABB import Point2D
+from src.model.boundingvolume.aabb.AABBWithMetrics import AABB2DWithMetrics
 
 if __name__ == "__main__":
+    bunny = DataLoader.load_binary_image("/Users/wenxinliu/Documents/thesis/thesis/data/2D/new_target_64x64.png")
+    normalised_bunny = DataLoader.normalise_train(bunny)
 
-    metricsRegistry = MetricsRegistry()
-    metricsRegistry.register_counter_metric("test")
-    metrics = metricsRegistry.get_metrics()
-    print(metrics)
+    metrics_registry = MetricsRegistry()
+
+    # cx: 31, cy: 30
+    # rx: 12, ry: 14
+
+    # TODO: double check the coordinates
+    # {'true_negative': 426.0, 'true_positive': 415.0}
+    aabb = AABB2DWithMetrics(
+        centre=Point2D(
+            x=31,
+            y=30
+        ),
+        radius=(14, 14),
+        data=normalised_bunny,
+        metrics_registry=metrics_registry
+    )
+
+    aabb.calculate_metrics()
+    print(metrics_registry.get_metrics())
+
+
