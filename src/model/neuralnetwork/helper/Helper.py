6e3bacb3f27185ad421b0c87643b0508cc8237b1
Wenxin Liu <unemployed_in_summertime@hotmail.com>
2023-06-21 02:42:01 +0100

    Add comments to helper method for calculating 3d hut initialisation configuration based on training image size

--- src/model/neuralnetwork/helper/Helper.py
+++ src/model/neuralnetwork/helper/Helper.py
@@ -4,11 +4,19 @@ def hut_3d_calculator(image_width, image_height):
 
     scaling = 64.0/float(image_width)
 
-    f = "f = max(0, -{}*X - {}*Y + 13)".format(0.2*scaling, 0.2*scaling) # -0.2*X - 0.2*Y + 13 calculated for 64x64 input
-    g = "g = max(0, {}*X + {}*Y - 13)".format(0.2*scaling, 0.2*scaling) # 0.2*X + 0.2*Y - 13 calculated for 64x64 input
-    h = "h = max(0, -{}*X + {}*Y + 5)".format(0.2*scaling, 0.2*scaling) # -0.2*X + 0.2*Y + 5 calculated for 64x64 input
-    j = "j = max(0, -{}*X + {}*Y)".format(0.4*scaling, 0.4*scaling) # -0.4*X + 0.4*Y + 0 calculated for 64x64 input
+    # -0.2*X - 0.2*Y + 13 calculated for 64x64 input
+    f = "f = max(0, -{}*X - {}*Y + 13)".format(0.2*scaling, 0.2*scaling)
 
+    # 0.2*X + 0.2*Y - 13 calculated for 64x64 input
+    g = "g = max(0, {}*X + {}*Y - 13)".format(0.2*scaling, 0.2*scaling)
+
+    # -0.2*X + 0.2*Y + 5 calculated for 64x64 input, make 5 bigger for a bigger hut, and smaller for a smaller hut
+    h = "h = max(0, -{}*X + {}*Y + 5)".format(0.2*scaling, 0.2*scaling)
+
+    # -0.4*X + 0.4*Y + 0 calculated for 64x64 input
+    j = "j = max(0, -{}*X + {}*Y)".format(0.4*scaling, 0.4*scaling)
+
+    # 3d hut
     k = "hut_3d = max(0, h - j - g - f)"
 
     print("for a training image of dimensions ({}, {}):\n".format(image_width, image_height))
