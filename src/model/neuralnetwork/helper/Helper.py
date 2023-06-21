be572a6f0747fd60248ccab218d10246c4c674ab
Wenxin Liu <unemployed_in_summertime@hotmail.com>
2023-06-21 02:35:04 +0100

    Implement helper method for calculating 3d hut initialisation configuration based on training image size

---
+++ src/model/neuralnetwork/helper/Helper.py
@@ -0,0 +1,19 @@
+def hut_3d_calculator(image_width, image_height):
+    if image_width is not image_height:
+        raise ValueError("training image width and height must be the same for this calculator")
+
+    scaling = 64.0/float(image_width)
+
+    f = "f = max(0, -{}*X - {}*Y + 13)".format(0.2*scaling, 0.2*scaling) # -0.2*X - 0.2*Y + 13 calculated for 64x64 input
+    g = "g = max(0, {}*X + {}*Y - 13)".format(0.2*scaling, 0.2*scaling) # 0.2*X + 0.2*Y - 13 calculated for 64x64 input
+    h = "h = max(0, -{}*X + {}*Y + 5)".format(0.2*scaling, 0.2*scaling) # -0.2*X + 0.2*Y + 5 calculated for 64x64 input
+    j = "j = max(0, -{}*X + {}*Y)".format(0.4*scaling, 0.4*scaling) # -0.4*X + 0.4*Y + 0 calculated for 64x64 input
+
+    k = "hut_3d = max(0, h - j - g - f)"
+
+    print("for a training image of dimensions ({}, {}):\n".format(image_width, image_height))
+    print(f)
+    print(g)
+    print(h)
+    print(j, "\n")
+    print(k)
