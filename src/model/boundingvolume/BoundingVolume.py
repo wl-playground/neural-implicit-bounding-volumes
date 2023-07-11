cebdeca087f9a1fd31f32a1d8b8fdeafee7053d6
Wenxin Liu <unemployed_in_summertime@hotmail.com>
2023-07-11 19:59:45 +0100

    Clean up unused code

--- src/model/boundingvolume/BoundingVolume.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from dataclasses import dataclass
-
-
-@dataclass
-class BoundingVolume(ABC):
-    @abstractmethod
-    def intersection_test(self, coordinate):
-        pass
